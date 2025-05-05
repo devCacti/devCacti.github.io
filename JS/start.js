6f6c4b7ed941e04a940ab29219120628a871f8f8
Tiago Laim <tiago.laim.rodrigues@gmail.com>
2025-05-05 12:08:42 +0200

    Main Page Introduction

---
+++ JS/start.js
@@ -0,0 +1,12 @@
+function triggerFullscreen() {
+    const elem = document.documentElement;
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { // Firefox
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { // Chrome, Safari and Opera
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { // IE/Edge
+        elem.msRequestFullscreen();
+    }
+}
